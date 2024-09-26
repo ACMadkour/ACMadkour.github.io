1135d77c332377a89018348ab33275f3639f2918
amadkour6 <amadkour6@gatech.edu>
2024-09-26 17:10:14 -0400

    WORKING ON CAROUSEL.  cleaned up index and styles

--- carousel.js
+++ carousel.js
@@ -1,10 +1,27 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; // Hide all slides
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; } // Loop back to the first slide
+
+    slides[slideIndex - 1].style.display = "block"; // Show the active slide
+
+    setTimeout(showSlides, 7000); // Change slide every 5 seconds
+}
+
 
 // Function to move to the next slide every 5 seconds (5000 milliseconds)
 let autoCycle = setInterval(() => {
     nextSlide();
-}, 5000); // Adjust this value to set the desired interval time
+}, 7000); // Adjust this value to set the desired interval time
 
 // Function for the next slide
 function nextSlide() {
@@ -16,30 +33,6 @@ function prevSlide() {
     showSlides(slideIndex -= 1);
 }
 
-// Function to show the current slide
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("carousel-slide");
-
-    // Reset the index if it exceeds the number of slides
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-
-    // Reset the index if it goes below 1
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    // Hide all slides
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Show the current slide
-    slides[slideIndex - 1].style.display = "block";
-}
-
 // Pause auto-cycling on hover
 document.querySelector('.carousel-container').addEventListener('mouseover', function() {
     clearInterval(autoCycle); // Stop auto-cycling on hover
@@ -49,5 +42,5 @@ document.querySelector('.carousel-container').addEventListener('mouseover', func
 document.querySelector('.carousel-container').addEventListener('mouseout', function() {
     autoCycle = setInterval(() => {
         nextSlide();
-    }, 5000); // Resume auto-cycling after hover
+    }, 7000); // Resume auto-cycling after hover
 });
