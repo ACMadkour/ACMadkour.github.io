1a6553bfb8441861c5a553ae4dff1ad148e4f1c0
amadkour6 <amadkour6@gatech.edu>
2024-09-29 22:43:17 -0400

    added slide buttons and got rid of transparency for carousel

--- carousel 2.js
+++ carousel 2.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let slides = document.querySelectorAll('.carousel__slide');
+// Initialize current slide index
     let currentSlide = 0;
+
+    let slides = document.querySelectorAll('.carousel__slide');
+    const leftBtn = document.querySelector('.left-btn');
+    const rightBtn = document.querySelector('.right-btn');
 
     // Hide all slides initially
     slides.forEach((slide, index) => {
@@ -14,6 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
         slides[currentSlide].style.display = 'flex'; // or 'block', based on your styling
     }
 
+    function showPreviousSlide() {
+        slides[currentSlide].style.display = 'none';
+        currentSlide = (currentSlide - 1) % slides.length;
+        slides[currentSlide].style.display = 'flex'; // or 'block', based on your styling
+    }
+
     // Set interval to auto-scroll slides (e.g., every 3 seconds)
     setInterval(showNextSlide, 10000);
+
+    // Add event listeners to the buttons
+    rightBtn.addEventListener('click', showNextSlide);
+    leftBtn.addEventListener('click', showPreviousSlide);
+
 });
+
